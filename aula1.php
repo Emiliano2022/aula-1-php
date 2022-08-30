85219cd633643e2e0953cdc3f0af64e61238ba09
gustavo emiliano <104604455+Emiliano2022@users.noreply.github.com>
2022-08-30 10:20:05 -0300

    Create aula1.php

---
+++ aula1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<body>
+    <?php
+echo "hello world"
+
+$variavel = 10 . "<br>";
+print $variavel;
+
+$variavel = "Gustavo". "<br>";
+print $variavel; 
+
+$variavel = 1.25 . "<br>"; 
+print $variavel;
+
+?>    
+</body>
+</html>
